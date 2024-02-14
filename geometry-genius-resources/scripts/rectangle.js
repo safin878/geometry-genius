089df224a07ceaee92a29bd8434d2c250317868c
safin878 <shafin6747@gmail.com>
2024-02-14 14:02:36 +0600

    All desing are completed but working first two card

--- geometry-genius-resources/scripts/rectangle.js
+++ geometry-genius-resources/scripts/rectangle.js
@@ -1,3 +1,18 @@
 function CalculateRectangleArea() {
-  console.log("calculate");
+  // get rectangle width value
+  const widthInput = document.getElementById("rectangle-width");
+  const Widthtext = widthInput.value;
+  const width = parseFloat(Widthtext);
+  console.log(width);
+  // get rectangle height value
+  const heightInput = document.getElementById("rectangle-height");
+  const heighttext = heightInput.value;
+  const height = parseFloat(heighttext);
+  console.log(height);
+  //   calculate rectangle area
+  const area = width * height;
+  console.log("Rectangle area is: " + area);
+  // display Triangle Area
+  const rectangleAreaSpan = document.getElementById("rectangle-area");
+  rectangleAreaSpan.innerText = area;
 }
